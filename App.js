38b46e7b57f2c11faf7a634b7cb5b0de4b98fd9c
Jeffrey William <118335821+Rikk1-Tikki-Tavi@users.noreply.github.com>
2023-12-21 22:21:34 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+// App.js
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from './firebase/firebase'; // Adjust the path to match your actual file structure
+import Signup from './components/Signup';
+import Login from './components/Login';
+import MealSearch from './components/MealSearch';
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [redirectToMealsearch, setRedirectToMeals] = useState(false);
+  const navigate = useNavigate();
+
+  const handleAuth = () => {
+    setUser(auth.currentUser);
+    setRedirectToMeals(true);
+  };
+
+  useEffect(() => {
+    if (redirectToMealsearch) {
+      navigate('/mealsearch');
+    }
+  }, [redirectToMealsearch, navigate]);
+
+  return (
+    <div>
+      {user ? (
+        <MealSearch />
+      ) : (
+        <>
+          <Login onAuth={handleAuth} />
+          <Signup onAuth={handleAuth} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
